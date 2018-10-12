873208bcc586c252c385f2cbe42695d3441b0019
The Gotamist <gotamist@gmail.com>
2018-10-12 15:59:33 -0700

    Finalize both models

--- models.py
+++ models.py
@@ -9,7 +9,6 @@ import torch.nn.init as I
 
 class Net(nn.Module):
 
-
     def __init__(self):
         super(Net, self).__init__()
         
@@ -22,7 +21,7 @@ class Net(nn.Module):
         # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
         # Input is 1,224,224
         self.conv1 = nn.Conv2d(1, 32, 5) # if stride=1, the size is (32,220,220)
-        I.kaiming_normal(self.conv1.weight)
+        I.kaiming_normal_(self.conv1.weight)
         self.conv1_bn = nn.BatchNorm2d(32)
         
         ## Note that among the layers to add, consider including:
@@ -82,4 +81,87 @@ class Net(nn.Module):
         
         # a modified x, having gone through all the layers of your model, should be returned
         return x
+    
+class Naimish_style_net(nn.Module):
+    ### https://arxiv.org/pdf/1710.00977.pdf
+    # That paper has a different size for the input image, so there are some differences, but I have tried to follow it closely
+
+    def __init__(self):
+        super(Naimish_style_net, self).__init__()
+        
+        ## TODO: Define all the layers of this CNN, the only requirements are:
+        ## 1. This network takes in a square (same width and height), grayscale image as input
+        ## 2. It ends with a linear layer that represents the keypoints
+        ## it's suggested that you make this last layer output 136 values, 2 for each of the 68 keypoint (x, y) pairs
+        
+        # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
+        # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
+        # Input is 1,224,224
+        self.conv1 = nn.Conv2d(1, 32, 4) # if stride=1, the size is (32,221,221)
+        I.kaiming_normal_(self.conv1.weight)
+        self.conv1_bn = nn.BatchNorm2d(32)
+        
+        ## Note that among the layers to add, consider including:
+        # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
+        # maxpool layer
+        # pool with kernel_size=2, stride=2
+        self.pool = nn.MaxPool2d(2, 2) #now it will be (32,110,110)
+        self.conv2 = nn.Conv2d(32, 64, 3) # now it is (64, 108,108 )
+        I.kaiming_uniform_(self.conv2.weight)
+        self.conv2_bn = nn.BatchNorm2d(64)
+        # After one more pool layer, it will be (64,54,54)
+        self.conv3 = nn.Conv2d(64, 128, 2) # now it is (128, 54,54 ).  After one more pooling, it should be (128,26,26)
+        I.kaiming_uniform_(self.conv3.weight)
+        self.conv3_bn = nn.BatchNorm2d(128)
+        self.conv4 = nn.Conv2d(128, 256, 1)# now it is (256, 26,26 ).  After one more pooling, it should be (256,13,13)
+        I.kaiming_uniform_(self.conv4.weight)
+        self.conv4_bn = nn.BatchNorm2d(256)
+        self.fc1 = nn.Linear( 256*13*13, 1000 )
+        I.kaiming_uniform_(self.fc1.weight)
+        # dropout with p=0.5
+        self.drop_layer1 = nn.Dropout(p=0.1)
+        self.drop_layer2 = nn.Dropout(p=0.2)
+        self.drop_layer3 = nn.Dropout(p=0.3)
+        self.drop_layer4 = nn.Dropout(p=0.4)
+        self.drop_layer5 = nn.Dropout(p=0.5)
+        self.drop_layer6 = nn.Dropout(p=0.6)
+        # finally, create 68*2 output channels (x and y for each of the 68 keytpoints)
+        self.fc2 = nn.Linear(1000, 1000)
+        I.kaiming_uniform_(self.fc2.weight)
+        self.fc3 = nn.Linear(1000, 68*2)
+        I.kaiming_uniform_(self.fc3.weight)
+        
+    def forward(self, x):
+        ## TODO: Define the feedforward behavior of this model
+        ## x is the input image and, as an example, here you may choose to include a pool/conv step:
+        ## x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv1_bn( self.conv1(x))) )# (32,110,110)
+        x = self.drop_layer1(x)
+#        print(x.shape)
+        x = self.pool(F.relu(self.conv2_bn( self.conv2(x))) )# (64,26,26)
+        x = self.drop_layer2(x)
+#        print(x.shape)
+        x = self.pool(F.relu(self.conv3_bn( self.conv3(x))) )# (128,12,12)
+        x = self.drop_layer3(x)
+#        print(x.shape)
+        x = self.pool(F.relu(self.conv4_bn(  self.conv4(x))) )# (256,2,2)
+        x = self.drop_layer4(x)
+#        print(x.shape)
+        # prep for linear layer by flattening 
+        x = x.view(x.size(0), -1)
+        
+        # two linear layers with dropout in between
+        x = F.relu(self.fc1(x))
+#        print(x.shape)
+        x = self.drop_layer5(x)
+#        print(x.shape)
+        x = F.relu(self.fc2(x))
+##        print(x.shape)
+        x = self.drop_layer6(x)
+#        print(x.shape)
+        x = self.fc3(x)
+        
+        # a modified x, having gone through all the layers of your model, should be returned
+        return x
+
 
